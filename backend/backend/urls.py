8246cf4329568ed0bf4144d28a8be79b5b03e394
james kiongo <jameskiongo@jamess-MacBook-Air.local>
2024-03-10 09:39:36 +0300

    finished backend

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -19,4 +19,6 @@ from django.urls import include, path
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("__reload__/", include("django_browser_reload.urls")),
+    path("api/v1/auth/", include("djoser.urls")),
+    path("api/v1/auth/", include("djoser.urls.jwt")),
 ]
